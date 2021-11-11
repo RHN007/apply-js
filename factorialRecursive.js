bcad7a7e10d83d562a66da5b307349e3e1496214
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2021-11-11 10:10:12 +0600

    adding fibonacci

--- factorialRecursive.js
+++ factorialRecursive.js
@@ -1,8 +1,17 @@
-//10! =
-for (var i = 10; i >= 1; i--) {}
+//10! = 1*2*3*4*5*6*7*8*9*10
+// for (var i = 10; i >= 1; i--) {}
 
-var i = 10;
+// var i = 10;
+// while (1 >= 1) {
+//   i--;
+// }
 
-while (1 >= 1) {
-  i--;
+function factorial(n) {
+  if (n == 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
 }
+var result = factorial(10);
+console.log(result);
