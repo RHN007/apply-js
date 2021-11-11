b1a2d9b1f32e4a5285e1eb7bf30112b90e446d03
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2021-11-11 10:11:03 +0600

    changing formula

--- fibonacci.js
+++ fibonacci.js
@@ -3,7 +3,7 @@
 // fibo[3] = fibo [3-1] + fibo[3-2];
 // fibo[4] = fibo [4-1] + fibo[4-2];
 // fibo[n] = fibo [n-1] + fibo[n-2];
-// fibo[i] = fibo [i-1] + fibo [n-2];
+// fibo[i] = fibo [i-1] + fibo [i-2];
 function fibonacci(n) {
   var fibo = [0, 1];
   for (var i = 2; i <= n; i++) {
