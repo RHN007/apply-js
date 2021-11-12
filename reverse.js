324bd34a6505284d205029adc0e52c759f081ddd
Rakibul Hassan Nayon <rakibul.nayon@outlook.com>
2021-11-12 09:04:36 +0600

    finalizing

--- reverse.js
+++ reverse.js
@@ -2,7 +2,12 @@ function reverseString(str) {
   var reverse = " ";
   for (var i = 0; i < str.length; i++) {
     var char = str[i];
+    reverse = char + reverse;
   }
+  return reverse;
 }
 
 var statement = "Hello Alien bhai brother";
+
+var forAllien = reverseString(statement);
+console.log(forAllien);
